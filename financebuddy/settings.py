821cb9e0c196b071f89e96a73d0492d58b1b91da
Bohdan-Diach <Bohdan.Diachenko@kname.edu.ua>
2026-06-11 10:40:45 +0000

    Виправив помилку STATIC_ROOT для Render

--- financebuddy/settings.py
+++ financebuddy/settings.py
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Додай ці рядки в кінець settings.py
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
